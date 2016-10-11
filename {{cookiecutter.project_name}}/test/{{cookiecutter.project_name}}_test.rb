3551edd019bf09fbbb84206f10ead0b7cf3a53fe
Nicolas ZERMATI <nicoolas25@gmail.com>
2016-10-11 20:10:34 +0200

    Uses the minitest/spec DSL

--- {{cookiecutter.project_name}}/test/{{cookiecutter.project_name}}_test.rb
+++ {{cookiecutter.project_name}}/test/{{cookiecutter.project_name}}_test.rb
@@ -1,11 +1,11 @@
 require "test_helper"
 
-class {{cookiecutter.module_name}}Test < Minitest::Test
-  def test_that_it_has_a_version_number
+describe {{cookiecutter.module_name}} do
+  it "has a version number" do
     value({{cookiecutter.module_name}}::VERSION).wont_be_nil
   end
 
-  def test_it_does_something_useful
-    assert false
+  it "does something useful" do
+    value(false).must_equal true
   end
 end
